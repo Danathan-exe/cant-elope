584e762eb517132796d6af0e717de5a46f4c3ccf
david <davidjdebonis@gmail.com>
2022-07-14 21:23:53 -0600

    while loop

--- fn.py
+++ fn.py
@@ -5,7 +5,17 @@ while True:
         entries = int(input("How many entries would you like to submit?"))
         break
     except:
-        print("This entry is invalid")
+        print("This entry is invalid. Please enter an integer.")
     
+counter = 0
+while counter < entries:
+    print(counter+ 1)
+    counter += 1
+    
+    
+#first_names = []
+#last_names[]
+#number_of_children = []
 
+#for i in range(entries):
     
