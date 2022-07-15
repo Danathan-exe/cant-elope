9f56a73bd59d7f445ceb09967bff4e90c4aaa352
david <davidjdebonis@gmail.com>
2022-07-14 21:33:17 -0600

    add to while loop

--- fn.py
+++ fn.py
@@ -1,21 +1,31 @@
 import pandas as pd
 
-while True:
+while True:las
     try:
         entries = int(input("How many entries would you like to submit?"))
         break
     except:
-        print("This entry is invalid. Please enter an integer.")
+        print("This entry is invalid. Please enter an integer.")   
     
-counter = 0
-while counter < entries:
-    print(counter+ 1)
+# make lists
+first_names = []
+last_names[]
+number_of_children = []
+
+# counter for loop
+counter = 1
+while counter <= entries:
+    _first_name = input("First name for entry " + counter + ": ")
+    _last_name = input("Last name for entry " + counter + ": ")
+    _number_of_children = input("Number of Children for entry " + counter + ": ")
+    first_names.append(_first_name)
+    last_names.append(_last_name)
+    number_of_children.append(_number_of_children)
     counter += 1
     
-    
-#first_names = []
-#last_names[]
-#number_of_children = []
+
+file_name = input("Input the Filename: ")
+
 
 #for i in range(entries):
     
