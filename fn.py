2e406371eb2d4c19658a4ad839e8897d573b24eb
david <davidjdebonis@gmail.com>
2022-07-14 20:42:35 -0600

    first function

---
+++ fn.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+while True:
+    try:
+        entries = int(input("How many entries would you like to submit?"))
+        break
+    except:
+        print("This entry is invalid")
+    
+
+    
